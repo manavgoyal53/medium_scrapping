ce75242a67b5f9c67073c05228d87e565283988c
unknown <manavgoyal53@gmail.com>
2020-10-13 09:33:09 +0530

    some minor changes

--- proto_scraping/proto_scraping/settings.py
+++ proto_scraping/proto_scraping/settings.py
@@ -77,32 +77,32 @@ WSGI_APPLICATION = 'proto_scraping.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'proto',
-
-        'USER': 'admin',
-
-        'PASSWORD': 'asd#1234',
-
-        'HOST': 'localhost',
-
-        'PORT': '',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
+
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'proto',
+
+#         'USER': 'admin',
+
+#         'PASSWORD': 'asd#1234',
+
+#         'HOST': 'localhost',
+
+#         'PORT': '',
+
+#     }
+
+# }
 
 
 # Password validation
